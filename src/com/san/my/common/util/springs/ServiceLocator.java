d556ef196e537fbfa33b483682bc244b0c03dd66
santhosh.gandhe <santhosh.gandhe@d53fe574-09b6-11de-a021-8b77bb4f2096>
2009-03-09 13:05:23 +0000

    seeds service added

--- src/com/san/my/common/util/springs/ServiceLocator.java
+++ src/com/san/my/common/util/springs/ServiceLocator.java
@@ -3,6 +3,7 @@ package com.san.my.common.util.springs;
 import org.apache.log4j.Logger;
 
 import com.san.my.service.AccountService;
+import com.san.my.service.SeedsService;
 
 public class ServiceLocator {
 	static Logger logger = Logger.getLogger(ServiceLocator.class);
@@ -10,6 +11,11 @@ public class ServiceLocator {
 	public static AccountService getAccountService() throws Exception{
 		AccountService accountService = (AccountService)BeanLocatorFactory.getBean("accountService");
 		return accountService;
+	}
+	
+	public static SeedsService getSeedsService() throws Exception{
+		SeedsService seedsService = (SeedsService)BeanLocatorFactory.getBean("SeedsService");
+		return seedsService;
 	}
 	
 }
